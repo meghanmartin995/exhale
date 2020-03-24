77f4170fec5f0a00b40c736344adab55aa906d43
Meghan Martin <meghanxmartin@gmail.com>
2020-03-24 21:51:53 +0100

    adds views of stream

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-
+  require "opentok"
+  @@opentok = OpenTok::OpenTok.new ENV['TOKBOX_API_KEY'], ENV['TOKBOX_SECRET_KEY']
   def home
-    require "opentok"
-    opentok = OpenTok::OpenTok.new ENV['TOKBOX_API_KEY'], ENV['TOKBOX_SECRET_KEY']
-    session = opentok.create_session :archive_mode => :always, :media_mode => :routed
-    session_id = session.session_id
+    session = @@opentok.create_session :archive_mode => :always, :media_mode => :routed
+    @session_id = session.session_id
+    gon.session_id = @session_id
+    @token = @@opentok.generate_token(@session_id)
+    gon.token = @token
+    @api_key = session.api_key
+    gon.api_key = @api_key
   end
 end
